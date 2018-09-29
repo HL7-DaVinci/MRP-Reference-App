99c9bee55efecdf5671fcbad014c05564a6dbc24
Nikolai Schwertner <nschwertner@users.noreply.github.com>
2018-09-29 10:13:48 -0400

    Workaround for an out of date value set in HAPI

--- js/config.js
+++ js/config.js
@@ -91,7 +91,7 @@ if (!MRP) {
               "id": "TASKID",
               "status": "completed",
               "intent": "plan",
-              "priority": "normal",
+              //"priority": "normal",    /* Note: This is a workaround for a problem with HAPI validator not suporting the current value set */
               "code": {
                   "coding":[
                       {
